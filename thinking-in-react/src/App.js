0d999fa13240cbbf746fea1c622c55f25ba8487f
Juan Antonio Plaza <juanantonio@datil.com>
2023-04-28 20:38:52 -0500

    Add state to components with hooks

--- thinking-in-react/src/App.js
+++ thinking-in-react/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 const data = [
   { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
@@ -11,12 +12,32 @@ const data = [
 ]
 
 function App() {
+  const [inputValue, setValue] = useState("");
+
+  function handleChange(event) {
+    setValue(event.target.value)
+  }
+
   return (
     <div className="App">
-      <SearchBar />
-      <ProductTable />
+      <StatefulComponents
+        onInputChange={handleChange}
+        value={inputValue} />
+      {/* <SearchBar />
+      <ProductTable products={data} /> */}
     </div>
   );
+}
+
+function StatefulComponents({onInputChange, value}) {  
+
+  return (
+    <div>      
+      <input
+        value={value}
+        onChange={onInputChange} />
+    </div>
+  )
 }
 
 function SearchBar() {
@@ -33,26 +54,46 @@ function SearchBar() {
   );
 }
 
-function ProductTable() {
-  const tableHeader = ["Name", "Price"]
-  const prod = data[0]
+function ProductTable({products}) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      );
+    }
+    rows.push(
+      <ProductTableRow
+        {...product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
   return (
     <table>
       <thead>
-        <ProductTableHeader headers={tableHeader} />
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
       </thead>
-      <tbody>
-        { data.map(p => (<ProductTableRow {...p} />)) }
-      </tbody>
+      <tbody>{rows}</tbody>
     </table>
   );
 }
 
-function ProductTableHeader({headers}) {
-  const head = headers.map(header => (<th>{header}</th>))
+function ProductCategoryRow({ category }) {
   return (
     <tr>
-      { head }
+      <th colSpan="2">
+        {category}
+      </th>
     </tr>
   );
 }
